3335b9ec1de651af94ce168b9b46f8454c32c6b4
Hendrix Costa <hendrixcosta@gmail.com>
2020-10-26 14:57:26 -0300

    [ADD] validador para numero do sms e whatsapp

--- internal/sms_twilio/models/mail_thread.py
+++ internal/sms_twilio/models/mail_thread.py
@@ -10,30 +10,40 @@ from odoo import models
 class MailThread(models.AbstractModel):
     _inherit = 'mail.thread'
 
+    def valid_alternative_9number(self, info):
+        """Validar um numero alternativo"""
+        alternative_number = "+{}9{}".format(
+            re.sub('[^0-9]', '', info.get("number"))[:4],
+            re.sub('[^0-9]', '', info.get("number"))[-8:]
+        )
+        valid_number = phone_validation.phone_sanitize_numbers_w_record(
+            [alternative_number], info.get("partner"))
+
+        if valid_number.get(alternative_number).get("sanitized"):
+            return valid_number.get(alternative_number).get("sanitized")
+        return False
+
     def _sms_get_recipients_info(self, force_field=False, message_type="sms"):
-        """" Aceitar numeros com 9 a menos
-        """
+        """" Manipular numeros de destinatario"""
         recipients_info = \
             super(MailThread, self)._sms_get_recipients_info(force_field)
 
-        if not recipients_info.get(self.id).get("sanitized"):
-            info = recipients_info.get(self.id)
-            alternative_number = "+{}9{}".format(
-                re.sub('[^0-9]', '', info.get("number"))[:4],
-                re.sub('[^0-9]', '', info.get("number"))[-8:]
-            )
-            valid_number = phone_validation.phone_sanitize_numbers_w_record(
-                [alternative_number], info.get("partner"))
+        info = recipients_info.get(self.id)
 
-            if valid_number.get(alternative_number).get("sanitized"):
+        # Para SMS sempre Adicionar o número 9
+        if message_type == "sms":
+            s_number = self.valid_alternative_9number(info)
+            if s_number:
+                recipients_info.get(self.id).update(sanitized=s_number)
+                return recipients_info
 
-                if message_type == "whatsapp":
-                    w_number = \
-                        "+{}".format(re.sub('[^0-9]', '', info.get("number")))
-                    recipients_info.get(self.id).update(sanitized=w_number)
+        # Para whatsapp remover espaços em branco
+        if message_type == "whatsapp":
+            w_number = "+{}".format(re.sub('[^0-9]', '', recipients_info.get(self.id).get("sanitized")))
 
-                elif message_type == "sms" :
-                    recipients_info.get(self.id).update(
-                        sanitized=alternative_number)
+            valid_number = self.valid_alternative_9number(info)
+            if valid_number:
+                recipients_info.get(self.id).update(sanitized=w_number)
+                return recipients_info
 
         return recipients_info
