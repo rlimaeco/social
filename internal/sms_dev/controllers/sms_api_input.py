34572039342860816ae87771000d6c5d88b96861
Hendrix Costa <hendrixcosta@gmail.com>
2020-11-01 16:38:33 -0300

    [FIX] Receive message from smsdev

--- internal/sms_dev/controllers/sms_api_input.py
+++ internal/sms_dev/controllers/sms_api_input.py
@@ -1,9 +1,10 @@
 # Copyright (C) 2020 - SUNNIT dev@sunnit.com.br
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import http
-from odoo.http import route
+from odoo.addons.mass_mailing_base.tools import helpers
 
+from odoo import http
+from odoo.http import request, route
 
 
 class TwilioWebhooks(http.Controller):
@@ -21,8 +22,23 @@ class TwilioWebhooks(http.Controller):
             "refer": "XXXXXXX" // Referencia utiliza na mensagem de enviada
         }
         """
+        response = "Mensagem recebida"
+
         if post.get('message', False) and post.get('from', False):
-            pass
 
+            params_sms_id = {
+                "body":  post.get('message'),
+                "number": helpers.sanitize_mobile(post.get('from')),
+                "message_type": "sms",
+                "message_id": post.get("id"),
+                "state": "received",
+                "type": "input",
+            }
 
-
+            sms_id = request.env['sms.sms'].sudo().create(params_sms_id)
+            message = sms_id.find_and_attach_to_lead()
+            if message:
+                response = '200 OK - Odoo SUNNIT recebeu SMS'
+        else:
+            response = 'ERRO - durante a comunicação com o Odoo SUNNIT'
+        return str(response)
