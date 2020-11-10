0707221ac53e2d277fb42f38acd1b27d5f343323
Rafael Lima <rafaelslima.py@gmail.com>
2020-11-10 15:12:34 -0300

    [UPD] Remoção separação por abas do mailing.mailing

--- internal/mass_mailing_base/__manifest__.py
+++ internal/mass_mailing_base/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Mass Mailing Base',
     'summary': 'Base for mass mailing sms messages',
     'description': '',
-    'version': '13.0.0.4.1',
+    'version': '13.0.0.5.1',
     'author': 'SUNNIT',
     'category': 'Marketing/Email Marketing',
     'contributors': [
