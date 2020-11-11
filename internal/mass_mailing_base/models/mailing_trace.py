6662744f8dc48cbcfc0e9792f8fa8f9d5d97e806
Hendrix Costa <hendrixcosta@gmail.com>
2020-11-11 18:52:11 -0300

    [ADD] FUncao para gatilhos do marketing automation

--- internal/mass_mailing_base/models/mailing_trace.py
+++ internal/mass_mailing_base/models/mailing_trace.py
@@ -7,3 +7,17 @@ class MailingTrace(models.Model):
     _inherit = 'mailing.trace'
 
     trace_type = fields.Selection(selection_add=[('whatsapp', 'Whatsapp')])
+
+    def set_opened(self, mail_mail_ids=None, mail_message_ids=None):
+
+        traces = super(MailingTrace, self).set_opened(mail_mail_ids, mail_message_ids)
+
+        mailing_ids = self.env["mailing.mailing"].search([
+            ("campaign_id", "=", self.campaign_id.id),
+            ("trigger", "=", "message_opened"),
+        ])
+
+        if mailing_ids:
+            mailing_ids.action_send_mail(self.env[self.model].browse(self.res_id))
+
+        return traces
