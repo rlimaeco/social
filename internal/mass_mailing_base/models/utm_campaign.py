eb5dfd752635829ae3db33be06424e5907196f78
Rafael Lima <rafaelslima.py@gmail.com>
2020-11-12 17:06:35 -0300

    [UPD] Customizações Atividade e Campanha

--- internal/mass_mailing_base/models/utm_campaign.py
+++ internal/mass_mailing_base/models/utm_campaign.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models, api
+from odoo.addons.mass_mailing.models.mailing import MASS_MAILING_BUSINESS_MODELS
 
 
 class UtmCampaign(models.Model):
@@ -21,6 +22,31 @@ class UtmCampaign(models.Model):
     mailing_whatsapp_count = fields.Integer(
         string='Number of Mass Whatsapp',
         compute="_compute_mailing_whatsapp_count",
+    )
+    mailing_model_real = fields.Char(
+        compute='_compute_model',
+        string='Recipients Real Model',
+        default='mailing.contact',
+        required=True
+    )
+    mailing_model_id = fields.Many2one(
+        'ir.model',
+        string='Recipients Model',
+        domain=[('model', 'in', MASS_MAILING_BUSINESS_MODELS)],
+        default=lambda self: self.env.ref('mass_mailing.model_mailing_list').id
+    )
+    mailing_model_name = fields.Char(
+        related='mailing_model_id.model',
+        string='Recipients Model Name',
+        readonly=True,
+        related_sudo=True
+    )
+    mailing_domain = fields.Char(string='Domain', default=[])
+
+    contact_list_ids = fields.Many2many(
+        'mailing.list',
+        'mass_utm_campaign_list_rel',
+        string='Mailing Lists'
     )
 
     @api.depends('mailing_activities_ids')
@@ -45,6 +71,11 @@ class UtmCampaign(models.Model):
         for campaign in self:
             qty_activities = campaign.mailing_activities_ids.filtered(lambda m: m.mailing_type == 'mail')
             campaign.mailing_mail_count = len(qty_activities)
+
+    @api.depends('mailing_model_id')
+    def _compute_model(self):
+        for record in self:
+            record.mailing_model_real = (record.mailing_model_name != 'mailing.list') and record.mailing_model_name or 'mailing.contact'
 
     def action_create_mass_whatsapp(self):
         action = self.env.ref('mass_mailing.action_create_mass_mailings_from_campaign').read()[0]
