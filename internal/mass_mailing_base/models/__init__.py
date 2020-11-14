0af8331aa4e91291ab32652bd06c4392c59a08da
Hendrix Costa <hendrixcosta@gmail.com>
2020-11-13 22:50:04 -0300

    [FIX] Port gatilhos

--- internal/mass_mailing_base/models/__init__.py
+++ internal/mass_mailing_base/models/__init__.py
@@ -3,7 +3,6 @@ from . import mail_thread
 from . import mail_thread_phone
 from . import mail_followers
 from . import mailing_mailing
-from . import mailing_trace
 from . import sms_sms
 from . import sms_api
 from . import utm_campaign
