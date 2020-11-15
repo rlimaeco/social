c9bb5ef556f38050a926cb9935970fea48bb3e91
Hendrix Costa <hendrixcosta@gmail.com>
2020-11-15 18:42:51 -0300

    [ADD] Simplificar função

--- internal/sms_twilio/controllers/sms_api_input.py
+++ internal/sms_twilio/controllers/sms_api_input.py
@@ -1,43 +1,17 @@
 # Copyright (C) 2020 - SUNNIT dev@sunnit.com.br
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import logging
-
 from odoo.addons.mass_mailing_base.tools import helpers
 from twilio.twiml.messaging_response import MessagingResponse
 
-from odoo import http, fields
+from odoo import http
 from odoo.http import request, route
-
-_logger = logging.getLogger(__name__)
-
-# https://www.twilio.com/docs/sms/api/message-resource#message-status-values
-sms_state = {
-    "queued":"outgoing",
-    "accepted":"outgoing",
-    "sent":"sent",
-    "delivered":"sent",
-    "received":"sent",
-    "read":"read",
-    "failed":"error",
-    "undelivered":"canceled",
-}
-trace_sms_state = {
-    "queued":"scheduled",
-    "accepted":"scheduled",
-    "sent":"sent",
-    "delivered":"sent",
-    "received":"sent",
-    "read":"opened",
-    "failed":"bounced",
-    "undelivered":"exception",
-}
 
 
 class TwilioWebhooks(http.Controller):
 
     @route(['/twilio/input'], type='http', auth="none", methods=['GET', 'POST', 'OPTIONS'], cors="*", csrf=False)
-    def tw_input(self,  **post):
+    def twilio_input_message(self,  **post):
         """
         Webhoock para receber mensagens do whatsapp sandbox
         https://www.twilio.com/console/sms/whatsapp/sandbox
@@ -73,7 +47,6 @@ class TwilioWebhooks(http.Controller):
             sms_id = request.env['sms.sms'].sudo().create(params_sms_id)
             message = sms_id.find_and_attach_to_lead()
             if message:
-                sms_id.set_reply_mailing_trace()
                 response = '200 OK - Odoo SUNNIT recebeu SMS do Twilio'
         else:
             response = 'ERRO - durante a comunicação com o Odoo SUNNIT'
@@ -81,8 +54,8 @@ class TwilioWebhooks(http.Controller):
         message_response_twilio = MessagingResponse()
         return str(message_response_twilio.message(response))
 
-    @route('/twilio/MessageStatus',  type="http", auth="none", methods=['POST', 'OPTIONS'], cors="*", csrf=False)
-    def incoming_sms_status(self,  **post):
+    @route('/twilio/MessageStatus',  type="http", auth="none", methods=['POST','OPTIONS'], cors="*", csrf=False)
+    def twilio_incoming_sms_status(self,  **post):
         """ Status de mensagens via twilio """
         if post:
             message_sid = post.get('MessageSid')
@@ -92,29 +65,20 @@ class TwilioWebhooks(http.Controller):
                 sms_id = sms_sms_model.search([
                     ("message_id", "=", message_sid)])
                 if sms_id:
-                    trace = request.env['mailing.trace'].sudo().search([
-                        ('sms_sms_id_int', '=', sms_id.id)
-                    ])
+                    # https://www.twilio.com/docs/sms/api/message-resource#message-status-values
+                    if message_status in ["sent", "read"]:
+                        sms_id.state = "sent"
 
-                    trace_state = trace_sms_state.get(message_status)
-                    if trace and trace_state:
-                        trace.write({trace_state: fields.Datetime.now(), 'exception': False})
-                        _logger.info(f"TRACE MSG-SID: [{message_sid}] "
-                                      f"Alterou Estado para: ['{message_status} ' <-> '{trace_state}' ]")
-                    elif trace:
-                        trace.set_failed(failure_type=sms_id.IAP_TO_SMS_STATE[trace_state])
-                        _logger.error(f"Mensagem SID: [{message_sid}] "
-                                      f"com falha: {sms_id.IAP_TO_SMS_STATE[trace_state]}")
+                    if message_status in ["failed", "undelivered"]:
+                        sms_id.state = "error"
 
-                    old_state = sms_id.state
-                    new_state = sms_state.get(message_status)
-                    sms_id.state = new_state
-                    _logger.info(f"SMS: [{message_sid}] "
-                                 f"Alteração de Estado: {old_state} -> {new_state}")
-                elif message_status == 'failed':
-                    _logger.error(f"Não foi possível criar a mensagem (sms.sms),"
-                                  f" Erro = {post.get('ErrorMessage')}")
+                    if message_status in ["received"]:
+                        sms_id.state = "received"
+
+                    # State do mailing.trace que garante que whatsapp foi lido
+                    if message_status == "read":
+                        sms_id.set_opened()
 
         response = MessagingResponse()
-        response.message('Odoo SUNNIT recebeu a mensagem')
+        response.message('Odoo SUNNIT Atualizou status')
         return str(response)
